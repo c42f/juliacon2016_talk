89e173fb9f77caa033cd414a997457d95831d792
Chris Foster <chris.foster@roames.com.au>
2016-06-24 12:58:32 -0400

    Frantic last minute hacking :-)

---
+++ icp_demo.jl
@@ -0,0 +1,99 @@
+using FixedSizeArrays
+using Displaz
+using PointClouds
+using PointCloudMatching
+
+using NearestNeighbors
+
+typealias V3d Vec{3,Float64}
+
+#= Base.(:.+){FSA<:FixedArray}(A::Vector{FSA}, b::FSA) = [a+b for a in A] =#
+#= Base.(:.+){FSA<:FixedArray}(b::FSA, A::Vector{FSA}) = [b+a for a in A] =#
+
+foo(v) = V3d(v[1], v[2], 0.1*(v[1].^2+v[2].^2))
+
+function toroid(R1,R2,theta,phi)
+    v = [R1+R2*cos(phi),0,R2*sin(phi)]
+    ct,st = sin(theta),cos(theta)
+    V3d([ ct st 0;
+         -st ct 0;
+         0  0  1] * v)
+end
+
+N = 10000
+#c1 = PointCloud([foo(randn(V3d)) for _=1:N])
+#c2 = PointCloud([foo(randn(V3d)) .+ Vec(0,0,5) for _=1:N])
+R1 = 10.0
+R2 = 2.0
+c1 = PointCloud(V3d[0.1*rand(V3d)+toroid(R1, R2, 2π*rand(),2π*rand()) for _=1:N])
+c2 = PointCloud(V3d[0.1*rand(V3d)+toroid(R1, R2, π*rand(),π*rand())+V3d(1,1,1) for _=1:N÷4])
+# Diverges!
+# c2 = PointCloud(V3d[0.1*rand(V3d)+toroid(R1, R2, π*rand(),π*rand())+V3d(1,10,10) for _=1:N÷4])
+add_normals!(c1)
+add_normals!(c2)
+
+#clearplot()
+
+function icpdebug(c1, c2, iteration, T, ref_inds, inlier_inds)
+    println(T)
+    p1 = copy(positions(c1))
+    @fslice p1[:,:] .-= T
+    p2 = positions(c2)
+    n2 = normals(c2)
+    # Plot clouds
+    plot3d!(p1, label="Cloud1", color=[0.5,0.5,1], markershape='x')
+    #plot3d!(p1[inlier_inds], label="Cloud1", color=[0.5,0.5,1], markershape='x')
+    plot3d!(p2, label="Cloud2", color=[1,0.5,0.5], markershape='+')
+    # Plot matching line segments
+    matches = similar(p1, 2*length(inlier_inds))
+    for i=1:2:length(matches)
+        i1 = inlier_inds[div(i+1,2)]
+        i2 = ref_inds[i1]
+        matches[i] = p1[i1]
+        v = p2[i2] - p1[i1]
+        matches[i+1] = matches[i] + n2[i2]*dot(n2[i2],v)
+    end
+    plot3d!(matches, label="Matches", markershape='-', linebreak=2)
+    wait(KeyEvent("Space"))
+end
+
+Displaz.figure("ICP")
+register(c1, c2,
+    dist_type = :surface,
+    iterfunc = (i,T,inds,inlier_inds)->
+                icpdebug(c1,c2,i,T,inds,inlier_inds)
+)
+
+
+#=
+Displaz.event_loop(
+        KeyEvent("c")=>Void,
+        KeyEvent("q")=>Void,
+        KeyEvent("u")=>CursorPosition
+) do event, arg
+    @show event, arg
+    global c1, c2
+    if event == KeyEvent("q")
+        return false
+    elseif event == KeyEvent("c")
+        clearplot()
+    elseif event == KeyEvent("u")
+        register(c1, c2, lambda=2.5,
+                 iterfunc=(i,T,inds,inlier_inds)->icpdebug(c1,c2,i,T,inds,inlier_inds))
+        #=
+        arg::CursorPosition
+        p1 = positions(c1)
+        p2 = positions(c2)
+        @fslice p1[:,:] .+= 0.01*randn(3) + [0,0,0.01]
+        idx = find_closest_points(dpositions(c1), c2)
+        matches = similar(p1, 2*length(p1))
+        matches[1:2:end] = p1
+        matches[2:2:end] = p2[idx]
+        plot3d!(p1, label="Cloud1", color=[0.8,0.8,1], markershape='x')
+        plot3d!(p2, label="Cloud2", color=[1,0.8,0.8], markershape='+')
+        plot3d!(matches, label="Matches", markershape='-', linebreak=2)
+        #plot3d!([arg.pos...], markersize=[1], label="Cursor")
+        =#
+    end
+end
+=#
--- slides.jl
+++ slides.jl
@@ -2,25 +2,92 @@ using FixedSizeArrays
 using Displaz
 using PointClouds
 using PointCloudMatching
+using CoordinateTransformations
 
 using NearestNeighbors
 
 typealias V3d Vec{3,Float64}
 
-run(`displaz -script`)
-run(`displaz -toggleui`)
-run(`displaz -viewposition 0.66666 0.5 0`)
-run(`displaz -viewradius 0.866`)
-
-function showslide(slidenum)
-    filename="slides/rasters/slide-$slidenum.ply"
-    run(`displaz -label Slide $filename`)
+function genslide_ply(slidenum, trans)
+    texturefile = abspath("slides/rasters/slide-$(@sprintf("%02d", slidenum)).png")
+    vertex_data = [
+        0     0   0  0 1
+        4/3   0   0  1 1
+        4/3   1   0  1 0
+        0     1   0  0 0
+        4/3/2 1/2 0  0 0
+    ]
+    trans2 = trans ∘ Translation(-4/3/2, -1/2, 0)
+    for i=1:size(vertex_data,1)
+        vertex_data[i,1:3] = [transform(trans2, Point(vertex_data[i,1:3]...))...]
+    end
+    plyname = abspath("slides/rasters/slide-$(@sprintf("%02d", slidenum)).ply")
+    open(plyname, "w") do f
+        write(f,
+            """
+            ply
+            format ascii 1.0
+            comment TextureFile $texturefile
+            element vertex 5
+            property double x
+            property double y
+            property double z
+            property double u
+            property double v
+            element face 1
+            property list int int vertex_index
+            end_header
+            $(join([@sprintf("%.3f", x) for x in vec(vertex_data')], " "))
+            4 0 1 2 3
+            """
+        )
+    end
+    plyname
 end
 
-maxslides = 20
-slidenum = 1
+maxslides = 30
+slideposes = Dict(
+    2=> (Vec(318594.281000, 7358164.560005, 29), 154.318000),
+    3=> (Vec(389819.050910, 7281222.665877, 12.290000), 681.855143),
+    4=> (Vec(482417.480011, 7207376.740005, 12.940000), 248.705282),
+    5=> (Vec(436865.792499, 7244398.039191, 22.586765), 76.534792),
+    #5=> (Vec(482369.720001, 7207425.970001, 13.500000), 196.309295),
+    #2=> (Vec(318627.684999, 7358030.485001, 30.711000), 154.317704, Vec(0.0,90.0,0.0))
+    #2=> (Vec(318651.647996, 7358057.850998, 29.608999), 138.744194, Vec(-106.905765, 25.132797, 0))
+)
 
+slidecommands = Dict(
+    #2=> ()->run(`displaz -viewposition 318651.647996 7358057.850998 29.608999 -viewradius 138.744194 -viewangles -106.905765 25.132797 0`)
+)
+
+function showslide(slidenum)
+    angles = Vec(0.0,90.0,0.0)
+    pose = (Vec(0.0,0.0,0.0), 0.866)
+    if haskey(slideposes, slidenum)
+        pose = slideposes[slidenum]
+    end
+    (transvec, range) = pose
+    trans = Translation(transvec) ∘ #RotationYZ(deg2rad(angles[2])) ∘ RotationXY(deg2rad(angles[1]-90))) ∘
+            Translation(0.0, 0.0, range - 0.866)
+    filename = genslide_ply(slidenum, trans)
+    if haskey(slidecommands, slidenum)
+        slidecommands[slidenum]()
+    end
+    run(`displaz -viewposition $(transvec[1]) $(transvec[2]) $(transvec[3]) -viewradius $range -viewangles $(angles[1]) $(angles[2]) 0 -label Slide $filename`)
+end
+
+run(`displaz -script`)
+run(`displaz -toggleui`)
+#run(`displaz -viewposition 0.66666 0.5 0`)
+#run(`displaz -viewradius 0.866`)
+
+slidenum = 1
 showslide(slidenum)
+
+run(`displaz -script -label GladstoneLidar data/gladstone_houses.laz data/121116_050444.laz data/lines/Poles.ply data/lines/point_vernon.laz data/lines/Lines.ply data/icp/matched.ply data/icp/unmatched.ply`)
+
+
+
 
 Displaz.event_loop(
         KeyEvent("n")=>Void,
