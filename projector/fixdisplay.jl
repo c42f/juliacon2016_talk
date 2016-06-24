e4bfb0e717f6f1a4ebaa4f6603de0b7c5a37b6b2
Chris Foster <chris.foster@roames.com.au>
2016-06-24 03:14:09 -0400

    Refine fixdisplay.jl

--- projector/fixdisplay.jl
+++ projector/fixdisplay.jl
@@ -1,15 +1,21 @@
 using CoordinateTransformations
 using FixedSizeArrays
 
+if length(ARGS) < 1
+     error("Screen name not supplied")
+end
+
 resolution = Vec(1024,768)
-T = Translation(resolution/2)
-for θ = linspace(0,2π,100)
+T = Translation(resolution/3)
+for θ = linspace(0,1.9π,100)
     trans = T ∘ Rotation2D(θ) ∘ inv(T)
 
     # Hack out homogenous transform coeffs for xrandr
     H = [Array(trans.M) Array(trans.v);
          0     0        1]
     Hstr = join(map(x->@sprintf("%.2f",x), H'), ",")
-    run(`xrandr --output HDMI2 --transform $Hstr`)
-    sleep(0.1)
+    try
+         run(`xrandr --output $(ARGS[1]) --transform $Hstr`)
+    end
+    sleep(0.05)
 end
