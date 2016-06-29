f85775100db5d5ad8a2aa980a55cdcc6cbb946b9
Chris Foster <chris.foster@roames.com.au>
2016-06-29 11:05:50 +1000

    Ensure rasters directory is created

--- slides/build.sh
+++ slides/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Script to build slide textures for talk.
+
 latexmk -pdf juliacon_talk.tex
 
 pdftoppm -r 500 juliacon_talk.pdf rasters/slide
 
+mkdir -p rasters
+
 for i in rasters/slide*.ppm ; do
     convert $i -alpha on ${i%.ppm}.png
-    cat slidemesh.ply | sed -e "s/TEX/$(basename $i .ppm).png/" > ${i%.ppm}.ply
+    #cat slidemesh.ply | sed -e "s/TEX/$(basename $i .ppm).png/" > ${i%.ppm}.ply
 done
 
